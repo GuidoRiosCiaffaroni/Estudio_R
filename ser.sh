89b2e023a8cbba3e05b6e64dd55718764d2a829b
Guido Ríos Ciaffaroni <guidoriosciaffaroni@gmail.com>
2025-07-10 16:39:54 +0000

    2025_07_10_16_39_54

--- ser.sh
+++ ser.sh
@@ -1,16 +1,18 @@
 
 #!/bin/bash
 service apache2 restart
-service mariadb restart
+service mysql restart
+service cron restart
+#service mariadb restart
 #service memcached restart
 #service rysnc restart
-service ssh restart
-#service vsftpd restart
-#service proftpd restart
-#service php5-fpm restart
-service ufw restart
 #service ssh restart
 #service vsftpd restart
 #service proftpd restart
-Process "sudo ufw allow 80/tcp" 1 "Abrir puerto 80 en UFW"
-bar
+#service php5-fpm restart
+#service ufw restart
+#service ssh restart
+#service vsftpd restart
+#service proftpd restart
+#Process "sudo ufw allow 80/tcp" 1 "Abrir puerto 80 en UFW"
+#bar
