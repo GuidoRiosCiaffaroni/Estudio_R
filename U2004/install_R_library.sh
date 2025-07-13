9fa676abc2c6ea142d6593084b77cbc01fb8ef62
root <root@wordpress-1-vm.us-central1-a.c.kumekimen-445211.internal>
2025-07-13 02:53:27 +0000

    2025_07_13_02_53_27

--- U2004/install_R_library.sh
+++ U2004/install_R_library.sh
@@ -8,14 +8,20 @@
 
 #############################################################################################################
 # Instalar dependencias
-# sudo apt install -y libcurl4-openssl-dev 
-# sudo apt install -y libssl-dev 
-# sudo apt install -y libxml2-dev 
-# sudo apt install -y libgit2-dev
+sudo apt install -y libcurl4-openssl-dev 
+sudo apt install -y libssl-dev 
+sudo apt install -y libxml2-dev 
+sudo apt install -y libgit2-dev
+sudo apt install -y  libfontconfig1-dev
+sudo apt install -y libharfbuzz-dev
+sudo apt install -y libfribidi-dev
+
+
 
 #############################################################################################################
 
 #############################################################################################################
 # Instalacion de librerias
-sudo Rscript -e "install.packages(c('tidyverse'), repos='https://cloud.r-project.org/')"
+#sudo Rscript -e "install.packages(c('tidyverse'), repos='https://cloud.r-project.org/')"
+#sudo Rscript -e "install.packages(c('ggplot2'), repos='https://cloud.r-project.org/')"
 ############################################################################################################
