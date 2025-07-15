70e8d8a1a3dd1313b3198383ff690c8df119c8c8
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2025-07-14 21:36:22 -0400

    2025_07_14_21_36_12

--- R/analisis.R
+++ R/analisis.R
@@ -13,26 +13,26 @@ install.packages("quarto")
 # readr (lectura de archivos),
 # purrr (programación funcional),
 # tibble (estructuras de datos mejoradas).
-install.packages("tidyverse")
+#install.packages("tidyverse")
 #########################################################################
 
 #########################################################################
 #resumen estadístico rápido
 # La función principal skim() ofrece una alternativa enriquecida a summary()
-install.packages("skimr")
+#install.packages("skimr")
 #########################################################################
 
 #########################################################################
 # el sistema más popular en R para la creación de gráficos.
 # Se basa en la gramática de los gráficos (Grammar of Graphics)
 # Es el estándar en visualización de datos en R.
-install.packages("ggplot2")
+#install.packages("ggplot2")
 #########################################################################
 
 #########################################################################
 # extensión de ggplot2 que facilita la creación de gráficos multivariantes
 # Gráficos de pares (ggpairs()),
-install.packages("GGally")
+#install.packages("GGally")
 #########################################################################
 # paquete que convierte gráficos de ggplot2 o crea desde cero gráficos
 # Permite:
@@ -40,13 +40,13 @@ install.packages("GGally")
 # Desplazamiento,
 # Tooltips (etiquetas flotantes),
 # Exportación interactiva.
-install.packages("plotly")
+#install.packages("plotly")
 #########################################################################
 
 #########################################################################
 # Instala shiny, un framework de R para construir aplicaciones web interactivas.
-install.packages("shiny")
+#install.packages("shiny")
 #########################################################################
 
-install.packages("scatterplot3d")
-install.packages("rgl")
+#install.packages("scatterplot3d")
+#install.packages("rgl")
