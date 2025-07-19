0c22b30c056d1422589d859ebeefcee8106e3959
root <guidoriosciaffaroni@gmail.com>
2025-07-19 05:20:05 -0400

    2025_07_19_05_20_04

---
+++ PlugIn/copy.sh
@@ -0,0 +1,5 @@
+rm -rf /home/r/Estudio_R/PlugIn/import-csv/ 
+rm -rf /home/r/Estudio_R/PlugIn/sh-executor/ 
+
+cp /home/r/Estudio_R/PlugIn/import-csv/ /var/www/wordpress/wp-content/plugins/ -r
+cp /home/r/Estudio_R/PlugIn/sh-executor/ /var/www/wordpress/wp-content/plugins/ -r
